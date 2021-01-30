6587a6775a8ec1f3d8e0946dae6189f699501719
DeViS1303 <lushantima@gmail.com>
2021-01-30 11:00:16 +0200

    Adjusted post icon

--- src/components/contents/Profile/MyPosts/Post/Post.js
+++ src/components/contents/Profile/MyPosts/Post/Post.js
@@ -3,7 +3,7 @@ import post from "./Post.module.css";
 function Post() {
     return (
         <div className={post.item}>
-            <img src="https://archilab.online/images/1/123.jpg" alt="ava" className={post.image}/>
+            <div className={post.avatarWrap}><img src="https://archilab.online/images/1/123.jpg" alt="ava" className={post.avatar}/></div>
             <p className={post.text}>asdasd</p>
         </div>
     )
