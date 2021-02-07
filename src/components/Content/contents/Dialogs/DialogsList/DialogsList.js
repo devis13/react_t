5aebcc508385d1be401c13da4e975ff0eff299bd
DeViS1303 <lushantima@gmail.com>
2021-02-07 10:33:37 +0200

    Moved date into an array in the DialogsList component

--- src/components/Content/contents/Dialogs/DialogsList/DialogsList.js
+++ src/components/Content/contents/Dialogs/DialogsList/DialogsList.js
@@ -2,12 +2,20 @@ import Dialog from "./Dialog/Dialog";
 import styles from "./DialogsList.module.css";
 
 const DialogsList = (props) => {
+
+    const dialogDate = [
+        {name:"Andrey", id:"1"},
+        {name:"Ivan", id:"2" },
+        {name:"Tymur", id:"3"},
+        {name:"Roman", id:"4"},
+    ]
+
     return (
         <div className={styles.dialogsList + " " + props.className}>
-            <Dialog name="Andrey" id="1" className={styles.item}/>
-            <Dialog name="Ivan" id="2" className={styles.item}/>
-            <Dialog name="Tymur" id="3" className={styles.item}/>
-            <Dialog name="Roman" id="4" className={styles.item}/>
+            <Dialog name={dialogDate[0].name} id={dialogDate[0].id} className={styles.item}/>
+            <Dialog name={dialogDate[1].name} id={dialogDate[1].id} className={styles.item}/>
+            <Dialog name={dialogDate[2].name} id={dialogDate[2].id} className={styles.item}/>
+            <Dialog name={dialogDate[3].name} id={dialogDate[3].id} className={styles.item}/>
         </div>
     )
 }
